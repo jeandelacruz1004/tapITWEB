fb38257c680747d37bb24888c0941c1ea9376ad5
Glen Escuadra <glen.escuadra@g.msuiit.edu.ph>
2019-10-15 16:40:15 +0800

    Registration done
    currently redirects back to landing, no profiles yet

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,19 @@ from wtforms import StringField, IntegerField, PasswordField, BooleanField, Subm
 from wtforms.validators import DataRequired, Optional, Length, Email, NumberRange, EqualTo
 
 
+class Login(FlaskForm):
+    email = StringField("Email Address", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=50)])
+    remember = BooleanField("Remember Password")
+
+    submit = SubmitField("Log In")
+
+
 class SignUp(FlaskForm):
-    public_name = StringField("Name", validators=[DataRequired(), Length(min=2, max=50)])
+    first_name = StringField("First Name", validators=[DataRequired(), Length(min=2, max=50)])
+    last_name = StringField("First Name", validators=[DataRequired(), Length(min=2, max=50)])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    rfID = StringField("RFID", validators=[Optional(), IntegerField()])
+    rfID = StringField("RFID", validators=[Optional()])
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=50)])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+import requests, json
+
+from flask import Flask, render_template, session, redirect, request, url_for
+from app.forms import *
 
 tap = Flask(__name__)
 
 
 @tap.route("/")
 @tap.route("/landing")
-def hello():
+def welcome():
+    if "app" in session:
+        return redirect("home")
 
     return render_template("index.html")
+
+
+@tap.route("/signup", methods=["GET", "POST"])
+def signup():
+    if "app" in session:
+        return redirect("home")
+
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        rfID = request.form.get("rfID")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+
+        signup = requests.post("http://127.0.0.1:5000/user/",
+                               data={'public_id': first_name + " " + last_name,
+                                     'username': username,
+                                     'email': email,
+                                     'rfID': rfID,
+                                     'password': password})
+        return redirect(url_for('welcome'))
+
+    return render_template("signup.html")
