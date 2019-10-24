8094412f17201541b9f81e9e6c07931ae57290e3
febandrin <arrielfeb24@gmail.com>
2019-10-24 08:09:55 +0800

    updated login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import requests, json
 
-from flask import Flask, render_template, session, redirect, request, url_for
+from flask_mail import Message
+from flask import Flask, render_template, session, redirect, request, url_for, flash
 from app.forms import *
 
 tap = Flask(__name__)
@@ -52,48 +53,13 @@ def login():
         login = requests.post("http://127.0.0.1:5000/auth/login",
                                data={'email': email,
                                     'password': password})
+
         if login.status_code == 200:
-            return redirect(url_for('signup'))
+            return render_template("dash.html")
         else:
+            print("bbbbb")
+            flash("Log in unsuccessful.", "danger")
             return redirect(url_for("login"))
 
 
     return render_template("login.html")
-
-#@tap.route("/login", methods=['POST','GET'])
-#def login():
-#    if "app" in session:
-#        return redirect("home")
-
-#    print('wa kasulod')
-#    if request.method == "POST":
-        
-#        print('sulod')
-#        email = request.form.get("email") 
-#        password = request.form.get("password")
-
-        # if check_user.status_code == 200:
-#       if request.form.get("username") == ("admin"):
-#            print('sulod admin')
-#            login = requests.post("http://127.0.0.1:5000/auth/login",form={"email":email, "password":password})
-#            print(login.status_code)
-#            print("admin")
-#            if login.status_code == 400:
-#                print("Email or password is incorrect")  
-                
-#            else: 
-#                return redirect(url_for('signup'))
-
-#       elif request.form.get("admin") == ("false"):
-#            print('sulod useeer')
-#            login = requests.post("http://127.0.0.1:5000/auth/login",form={"email":email, "password":password})
-#            print(login.status_code)
-#            print("custom")
-#            if response.status_code == 400:
-#                print("Email or password is incorrect")
-                
-#            else:
-#                return redirect(url_for('welcome'))
-#                print("user not found")
-
-#    return render_template('login.html')
